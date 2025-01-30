696f42b489a13c20cb76c460b43134b71e3d84fa
Mateusz Ostróżka <154000222+Mateusz-34@users.noreply.github.com>
2025-01-30 13:54:51 +0100

    szablony_klas(silnia,iloczyn4liczb)

---
+++ szablony_klas(silnia,iloczyn4liczb).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+template <class Nasza_klasa>
+Nasza_klasa Silnia(Nasza_klasa liczba) {
+    if (liczba <= 1) return 1;
+    return liczba * Silnia(liczba - 1);
+}
+
+template <class Nasza_klasa>
+Nasza_klasa iloczyn_4_liczb(Nasza_klasa a, Nasza_klasa b, Nasza_klasa c, Nasza_klasa d) {
+    Nasza_klasa wynik = (a * b * c * d);
+    return wynik;
+}
+
+int main(int argc, const char * argv[]) {
+    int liczba1 = Silnia<int>(5);
+    cout << "Nasza silnia = " << liczba1 << endl;
+
+    int liczba2 = iloczyn_4_liczb<int>(5, 3, 4, 5);
+    cout << "Iloczyn 4 liczb = " << liczba2 << endl;
+
+    getchar();
+    return 0;
+}
